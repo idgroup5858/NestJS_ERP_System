ef30f3908ef5f42aaae7876223cf5d934ff5de1a
idgroup5858 <idgroup5858@gmail.com>
2026-04-10 14:57:52 +0500

    localhost

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,7 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     imports:[
         TypeOrmModule.forRoot({
             type:"mysql",
-            host:"109.196.103.18",
+            host:"localhost",
             port:3306,
             username:"root",
             password:"",
