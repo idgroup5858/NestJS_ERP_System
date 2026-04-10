473a2f2f645a7f3c2c496853ee48e8180d8d9dc1
idgroup5858 <idgroup5858@gmail.com>
2026-04-10 15:03:32 +0500

    root

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             host:"localhost",
             port:3306,
             username:"root",
-            password:"",
+            password:"root",
             database:"idgrouperp",
             autoLoadEntities:true,
             synchronize:true
